ef11e8b753635c2534bd299608f926731cbe62ca
Gnana Venkat Ram <venkat1217@gmail.com>
2026-04-28 13:45:09 +0530

    Add this code

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true
+        target: "https://venkatram1712.github.io/FSAD-backend-p",
+        changeOrigin: true,
+        secure: true
       }
     }
   }
